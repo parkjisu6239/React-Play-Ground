7aa5b7db2e7d57ce6a76aec10f34d68c14a37902
kate <parkjisu6239@gmail.com>
2022-08-06 14:35:39 +0900

    initial

---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+import menu from "@/constants/menu";
+
+const Header = () => {
+  return (
+    <div>
+      asdasd
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/constants/menu.ts
@@ -0,0 +1,19 @@
+const menu = [
+  {
+    id: "home",
+    path: "/",
+    name: "Home"
+  },
+  {
+    id: "about",
+    path: "/about",
+    name: "About"
+  },
+  {
+    id: "render-props",
+    path: "/render-props",
+    name: "Render Props"
+  }
+];
+
+export default menu;
